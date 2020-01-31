8fb5d1ccb093ec162e1de9c71241cefebc3340ce
Panco Cheong <panco.cheong@hotmail.com>
2020-01-31 22:12:26 +0800

    Initial Commit  - Walking Skeleton

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        // type prop to create property
+        public int Id { get; set; }
+        public string Name { get; set; }        
+    }
+}
